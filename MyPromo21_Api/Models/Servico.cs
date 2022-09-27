692c036f1237e094ddcb1a998e71ce478dd397e9
bruno.goncalves4 <bruno.goncalves4@ITELABD03>
2022-09-26 22:17:47 -0300

    .

---
+++ MyPromo21_Api/Models/Servico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPromo21_Api.Models
+{
+    public class Servico
+    {
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string LinkImagem { get; set; }
+    }
+}
---
+++ MyPromo21_Api/ViewModels/ServicoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MyPromo21_Api.Dtos;
+using MyPromo21_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPromo21_Api.ViewModels
+{
+    public class ServicoViewModel
+    {
+        public class CreateServicoViewModel
+        {
+            public Servico Servico { get; set; }
+        }
+
+        public class DeleteServicoViewModel
+        {
+            public int Id { get; set; }
+        }
+        public class UpdateServicoViewModel
+        {
+            public ServicoDto Servico { get; set; }
+        }
+    }
+}
