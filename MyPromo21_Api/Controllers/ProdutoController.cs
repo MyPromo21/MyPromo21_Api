f090d1073fe2dbf260b8865b6070e223e0a1fcbd
bruno.goncalves4 <bruno.goncalves4@ITELABD03>
2022-09-26 21:21:09 -0300

    .

---
+++ MyPromo21_Api/Controllers/ProdutoController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyPromo21_Api.Repositories;
+using MyPromo21_Api.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static MyPromo21_Api.ViewModels.ProdutoViewModel;
+
+namespace MyPromo21_Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoRepository _produtoRepository;
+
+        public ProdutoController()
+        {
+            _produtoRepository = new ProdutoRepository();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduto(ProdutoViewModel produto)
+        {
+            if (produto.Produto == null) return Ok("Parâmetros informados incorretamente!");
+
+            var result = _produtoRepository.CreateProduto(produto.Produto);
+
+            if (result) return Ok("Produto cadastrado com sucesso!");
+
+            return Ok("Não foi possível cadastrar o produto!");
+        }
+        [HttpPut]
+        public IActionResult UpdateProduto(UpdateProdutoViewlModel produto)
+        {
+            if (produto.Produto == null) return Ok("Parâmetros informados incorretamente!");
+
+            var result = _produtoRepository.UpdateProduto(produto.Produto);
+
+            if (result) return Ok("Produto atualizado com sucesso!");
+
+            return Ok("Não foi possível atualizar o produto!");
+
+        }
+        [HttpDelete]
+        public IActionResult DeleteProduto(ProdutoDeleteViewModel deleteProdutoViewModel)
+        {
+            if (deleteProdutoViewModel.IdProduto == 0) return Ok("Id do produto inválido!");
+
+            var result = _produtoRepository.DeleteProduto(deleteProdutoViewModel);
+
+            if (result) return Ok("Produto excluído com sucesso!");
+
+            return Ok("Não foi possível excluir o produto!");
+        }
+        [HttpGet]
+        public IActionResult GetProduto(string descricao)
+        {
+            if (string.IsNullOrWhiteSpace(descricao)) return Ok("Parâmetro inválido!");
+
+            var produto = _produtoRepository.GetProduto(descricao);
+
+            if (produto != null) return Ok(produto);
+
+            return Ok("Produto não encontrado!");
+        }
+    }
+}
--- MyPromo21_Api/Repositories/ProdutoRepository.cs
+++ MyPromo21_Api/Repositories/ProdutoRepository.cs
@@ -1,9 +1,13 @@
-﻿using MyPromo21_Api.ViewModels;
+﻿using Dapper;
+using MyPromo21_Api.Dtos;
+using MyPromo21_Api.Models;
+using MyPromo21_Api.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using static MyPromo21_Api.ViewModels.ProdutoViewModel;
 
 namespace MyPromo21_Api.Repositories
 {
@@ -19,7 +23,7 @@ namespace MyPromo21_Api.Repositories
             }
         }
 
-        public bool CreateProduto(ProdutoViewModel produto)
+        public bool CreateProduto(Produto produto)
         {
             var result = false;
 
@@ -27,15 +31,99 @@ namespace MyPromo21_Api.Repositories
             {
                 using (_conexaoBanco)
                 {
-                    var query = "insert into Produto() values()";
+                    var query = "insert into Produto(Descricao,Preco,Quantidade,Perecivel,ValidadeProduto,LinkImagem) " +
+                        "Values(@descricao,@preco,@quantidade,@perecivel,@validadeProduto,@linkImagem)";
+                    var parameters = new
+                    {
+                        produto.Descricao,
+                        produto.Preco,
+                        produto.Quantidade,
+                        produto.Perecivel,
+                        produto.ValidadeProduto,
+                        produto.LinkImagem
+                    };
+                    _conexaoBanco.Query(query, parameters);
+                    result = true;
                 }
             }
             catch (SqlException e)
             {
-                
+                result = false;
             }
 
             return result;
         }
+        public bool UpdateProduto(ProdutoDto produto)
+        {
+            var result = false;
+
+            try
+            {
+                using (_conexaoBanco)
+                {
+                    var query = "update Produto set Descricao = @descricao,Preco = @preco,Quantidade = @quantidade,Perecivel = @perecivel,ValidadeProduto = @validadeProduto," +
+                        "LinkImagem = @linkImagem where Id = @id";
+                    var parameters = new
+                    {
+                        produto.Descricao,
+                        produto.Preco,
+                        produto.Quantidade,
+                        produto.Perecivel,
+                        produto.ValidadeProduto,
+                        produto.Id
+                    };
+                    _conexaoBanco.Query(query,parameters);
+                    result = true;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public Produto GetProduto(string descricao)
+        {
+            var produto = new Produto();
+
+            try
+            {
+                var query = "select * from Produto where Descricao = @descricao";
+                var parameters = new
+                {
+                    descricao
+                };
+
+                produto = _conexaoBanco.QueryFirstOrDefault(query,parameters);
+            }
+            catch (SqlException e)
+            {
+                produto = null;
+            }
+            return produto;
+        }
+        public bool DeleteProduto(ProdutoDeleteViewModel IdProduto)
+        {
+            var result = false;
+
+            try
+            {
+                using (_conexaoBanco)
+                {
+                    var query = "delete from Produto where IdProduto = @IdProduto";
+                    var parameters = new { IdProduto.IdProduto };
+                    _conexaoBanco.Query(query,parameters);
+                    result = true;
+                }
+            }
+            catch (SqlException e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
     }
 }
